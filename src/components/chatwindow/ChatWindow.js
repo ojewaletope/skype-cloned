34b24fad5acd47a2bdace83d951de2693d81d307
Tope Ojewale <topeojewale@gmail.com>
2018-06-27 13:53:43 +0100

    Added Chat component

--- src/components/chatwindow/ChatWindow.js
+++ src/components/chatwindow/ChatWindow.js
@@ -1,13 +1,18 @@
 import React from "react";
-import store from '../../store'
-import Header from '../header/Header'
+import store from "../../store";
+import Header from "../header/Header";
+import _ from "lodash";
+import Chats from "../../containers/chats/Chats";
+import './ChatWindow.css'
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
-  const activeUser = state.contacts[activeUserId]
+  const activeUser = state.contacts[activeUserId];
+  const activeMsgs = state.messages[activeUserId];
   return (
     <div className="ChatWindow">
-        <Header user={activeUser}/>
+      <Header user={activeUser} />
+      <Chats messages={_.values(activeMsgs)} />
     </div>
   );
 };
---
+++ src/containers/chats/Chats.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Chats.css'
+
+const Chat = ({ message }) => {
+  const { text, is_user_msg } = message;
+  return (
+    <span className={`Chat ${is_user_msg ? "is-user-msg" : ""}`}>{text}</span>
+  );
+};
+
+class Chats extends React.Component {
+  render() {
+    return (
+      <div className="Chats">
+        {this.props.messages.map(message => {
+          return <Chat message={message} key={message.number} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Chats;
